39d88dbc4ceb18a6ef528a73ef6b4f378c0a9cce
昭召 <lz96297@alibaba-inc.com>
2017-05-17 00:15:00 +0800

    添加音乐播放
    添加音乐播放

--- ReaderMovie/data/localDatabase.js
+++ ReaderMovie/data/localDatabase.js
@@ -1,7 +1,7 @@
 var posts = [
   {
     avatar: "/images/avatar/1.png",
-    author:"柳永",
+    author: "柳永",
     date: "May 12 2017",
     title: "雨霖铃-柳永",
     imageUrl: "/images/post/cat.png",
@@ -11,6 +11,11 @@ var posts = [
     datetime: "三小时前",
     likeCount: 80,
     viewCount: 98,
+    music: {
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      title: "此时此刻-许巍",
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    },
     postId: 0
   },
   {
@@ -25,6 +30,11 @@ var posts = [
     datetime: "三小时前",
     likeCount: 80,
     viewCount: 98,
+    music: {
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      title: "此时此刻-许巍",
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    },
     postId: 1
   },
   {
@@ -39,6 +49,11 @@ var posts = [
     datetime: "三小时前",
     likeCount: 80,
     viewCount: 98,
+    music: {
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      title: "此时此刻-许巍",
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    },
     postId: 2
   },
   {
@@ -53,6 +68,11 @@ var posts = [
     datetime: "三小时前",
     likeCount: 80,
     viewCount: 98,
+    music: {
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      title: "此时此刻-许巍",
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    },
     postId: 3
   },
   {
@@ -67,6 +87,11 @@ var posts = [
     datetime: "三小时前",
     likeCount: 80,
     viewCount: 98,
+    music: {
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      title: "此时此刻-许巍",
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    },
     postId: 4
   }
 ]
--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isPlaying: false
   },
 
   /**
@@ -16,31 +16,31 @@ Page({
     var postId = options.id;
     var postData = postsData.postList[postId];
     this.setData({
-      postData:postData
+      postData: postData
     })
     this.data.postId = postId
     var postsCollected = wx.getStorageSync('posts_collected')
-    if(postsCollected){
+    if (postsCollected) {
       var postCollected = postsCollected[postId]
       this.setData({
         collected: postCollected
-      }) 
+      })
     } else {
       var postsCollected = {}
       postsCollected[postId] = false
       wx.setStorageSync('posts_collected', postsCollected)
     }
-       
+
   },
 
-  onCollectionTap: function() {
+  onCollectionTap: function () {
     var postsCollected = wx.getStorageSync('posts_collected')
     var postCollected = postsCollected[this.data.postId]
     postCollected = !postCollected
     postsCollected[this.data.postId] = postCollected
     wx.setStorageSync('posts_collected', postsCollected)
     this.setData({
-      collected:postCollected
+      collected: postCollected
     })
     var title = postCollected ? '收藏成功' : '取消成功';
     wx.showToast({
@@ -48,66 +48,94 @@ Page({
     })
   },
 
+  onShareTap: function () {
+    console.log('onShareTap')
+    wx.showActionSheet({
+      itemList: ['分享给微信好友', '分享到朋友圈', '分享到QQ', '分享到微博']
+    })
+  },
+
+  onMusicTap: function () {
+    if (this.data.isPlaying) {
+      this.setData({
+        isPlaying: false
+      })
+      wx.pauseBackgroundAudio()
+    } else {
+      this.setData({
+        isPlaying: true
+      })
+      console.log(this.data.postData.music)
+      wx.playBackgroundAudio(this.data.postData.music)
+      // wx.playBackgroundAudio({
+      //   dataUrl: this.postData.music.dataUrl,
+      //   title: this.postData.music.title,
+      //   coverImgUrl: this.postData.music.coverImgUrl
+      // })
+
+    }
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   }
 })
