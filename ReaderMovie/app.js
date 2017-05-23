caa99110126c0197e860f521736ccb7d83084d13
lizhaobomb <lizhaobomb@163.com>
2017-05-24 00:15:50 +0800

    电影页面细节调整
    添加网络请求

--- ReaderMovie/app.js
+++ ReaderMovie/app.js
@@ -2,5 +2,6 @@ App({
   globalData:{
     g_isPlaying: false,
     g_currentId: 0,
+    doubanBase: "https://api.douban.com"
   }
 })
--- ReaderMovie/pages/movies/movie.js
+++ ReaderMovie/pages/movies/movie.js
@@ -1,66 +1,87 @@
 // movie.js
+
+var app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var in_theatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters"
+    var coming_soonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon"
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250"
+   console.log(in_theatersUrl,coming_soonUrl,top250Url)
+    this.getMovieListData(in_theatersUrl)
+    this.getMovieListData(coming_soonUrl)
+    this.getMovieListData(top250Url)
+  },
+
+  getMovieListData: function (url) {
+    wx.request({
+      url: url,
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (error) {
+        console.log(error)
+      }
+    }) 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
